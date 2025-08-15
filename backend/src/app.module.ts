a8dfc09427509d403e4a5f2ee7d1fc20c1cc0061
= <=>
2025-08-15 08:41:19 +0200

    config orm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
 import { Logger } from 'winston';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.POSTGRES_URL,
+      autoLoadEntities: true,
+      synchronize: process.env.ENV !== 'production',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, Logger],
 })
